a14dc109ad5bb066310302931108edf7e5810ac8
Eduardo Burgoa <eduardo.burgoa@gmail.com>
2019-11-20 00:56:23 +0100

    model working with r-squared 0.75

---
+++ utils.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import numpy as np
+from collections import defaultdict
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, mean_squared_error
+import matplotlib.pyplot as plt
+
+def coef_weights(lm_model, X_train):
+    '''
+    INPUT:
+    coefficients - the coefficients of the linear model 
+    X_train - the training data, so the column names can be used
+    OUTPUT:
+    coefs_df - a dataframe holding the coefficient, estimate, and abs(estimate)
+    
+    Provides a dataframe that can be used to understand the most influential coefficients
+    in a linear model by providing the coefficient estimates along with the name of the 
+    variable attached to the coefficient.
+    '''
+    coefs_df = pd.DataFrame()
+    coefs_df['est_int'] = X_train.columns
+    coefs_df['coefs'] = lm_model.coef_
+    coefs_df['abs_coefs'] = np.abs(lm_model.coef_)
+    coefs_df = coefs_df.sort_values('abs_coefs', ascending=False)
+    return coefs_df
+
+
+def find_optimal_lm_mod(X, y, cutoffs, test_size = .30, random_state=42, plot=True):
+    '''
+    INPUT
+    X - pandas dataframe, X matrix
+    y - pandas dataframe, response variable
+    cutoffs - list of ints, cutoff for number of non-zero values in dummy categorical vars
+    test_size - float between 0 and 1, default 0.3, determines the proportion of data as test data
+    random_state - int, default 42, controls random state for train_test_split
+    plot - boolean, default True, True to plot result
+
+    OUTPUT
+    r2_scores_test - list of floats of r2 scores on the test data
+    r2_scores_train - list of floats of r2 scores on the train data
+    lm_model - model object from sklearn
+    X_train, X_test, y_train, y_test - output from sklearn train test split used for optimal model
+    '''
+    r2_scores_test, r2_scores_train, num_feats, results = [], [], [], dict()
+    for cutoff in cutoffs:
+
+        #reduce X matrix
+        reduce_X = X.iloc[:, np.where((X.sum() > cutoff) == True)[0]]
+        num_feats.append(reduce_X.shape[1])
+
+        #split the data into train and test
+        X_train, X_test, y_train, y_test = train_test_split(reduce_X, y, test_size = test_size, random_state=random_state)
+
+        #fit the model and obtain pred response
+        lm_model = LinearRegression(normalize=True)
+        lm_model.fit(X_train, y_train)
+        y_test_preds = lm_model.predict(X_test)
+        y_train_preds = lm_model.predict(X_train)
+
+        #append the r2 value from the test set
+        r2_scores_test.append(r2_score(y_test, y_test_preds))
+        r2_scores_train.append(r2_score(y_train, y_train_preds))
+        results[str(cutoff)] = r2_score(y_test, y_test_preds)
+
+    if plot:
+        plt.plot(num_feats, r2_scores_test, label="Test", alpha=.5)
+        plt.plot(num_feats, r2_scores_train, label="Train", alpha=.5)
+        plt.xlabel('Number of Features')
+        plt.ylabel('Rsquared')
+        plt.title('Rsquared by Number of Features')
+        plt.legend(loc=1)
+        plt.show()
+
+    best_cutoff = max(results, key=results.get)
+
+    #reduce X matrix
+    reduce_X = X.iloc[:, np.where((X.sum() > int(best_cutoff)) == True)[0]]
+    num_feats.append(reduce_X.shape[1])
+
+    #split the data into train and test
+    X_train, X_test, y_train, y_test = train_test_split(reduce_X, y, test_size = test_size, random_state=random_state)
+
+    #fit the model
+    lm_model = LinearRegression(normalize=True)
+    lm_model.fit(X_train, y_train)
+
+    return r2_scores_test, r2_scores_train, lm_model, X_train, X_test, y_train, y_test
